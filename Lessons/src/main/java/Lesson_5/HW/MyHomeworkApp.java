cf078cca3bec2414c1d2d97abb0e118e216f7113
Dr.Ratios <AleksGOlds7@yandex.ru>
2024-07-23 19:25:38 +0300

    Lesson_5

--- Lessons/src/main/java/Lesson_5/HW/MyHomeworkApp.java
+++ Lessons/src/main/java/Lesson_5/HW/MyHomeworkApp.java
@@ -46,11 +46,19 @@ public class MyHomeworkApp {
         Thread t2 = new Thread(new Runnable() {
             @Override
             public synchronized void run() {
+                try {
+                    t.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
                 for (int i = 0; i < h; i++) {
                     a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
             }
         });
+        t.start();
+        t2.start();
+
 
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h, h);
